594d8162e2aaecbb0de3439b3654a14d14b8b27a
AK_333 <connect.akashkakadiya@gmail.com>
2024-06-08 11:18:08 +0530

    folder Structure

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ app.listen(port, () => {
     setInterval(() => {
         console.log("1 sec Server is running at", port);
     }, 1000);
-    setInterval(() => {
-        console.log("2 sec Server is running at", port);
-    }, 2000);
     setTimeout(() => {
         console.log("1 sec TimeOut Server is running at", port);
     }, 1000);
